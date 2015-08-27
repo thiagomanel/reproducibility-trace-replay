06135b2e7534b78bbe09300fe9fa60d37981b1ff
Heitor Meira <htrmeira@gmail.com>
2015-08-27 01:36:01 -0300

    changing structure of replayer script, adding sleep parameter and changing status reporter

---
+++ lib/helpers.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+check_parameter() {
+	if [ "$#" -ne 2 ]; then
+		echo "$1 is missing"
+		exit 1
+	fi
+}
+
+success_or_die() {
+	if [ "$?" -ne "0" ]; then
+		exit 1;
+	fi
+}
+
+echo_status_ok() {
+	if [ "$TERM" != "dumb" ];then
+		echo -e " \e[32mOK($1)"; tput sgr0
+	else
+		echo -e " OK($1)"
+	fi
+}
+
+echo_status_fail() {
+	if [ "$TERM" != "dumb" ];then
+		echo -e " \e[31mFAIL($1)"; tput sgr0
+	else
+		echo -e " FAIL($1)"
+	fi
+}
+
+echo_status() {
+	if [ "$1" -ne "0" ]; then
+		echo_status_fail $1
+	else
+		echo_status_ok $1
+	fi
+}
